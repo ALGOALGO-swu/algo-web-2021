244b4a869e8b7757c89e1f8b7d110c000a4a18d6
JJ503 <63184334+JJ503@users.noreply.github.com>
2021-02-09 00:44:45 +0900

    change my_settings.py

--- Boss/Boss/settings.py
+++ Boss/Boss/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_fd15ze=qipd2^hg+^$iomb2py9#0fh1#!zr5_8na^a6xzill9'
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,17 +82,7 @@ WSGI_APPLICATION = 'Boss.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'algoalgo',
-        'USER': 'algoalgo-2021-staff',
-        'PASSWORD': 'welcome-to-algoalgo-gcp',
-        'HOST': '34.64.120.154',
-        'PORT': '3306',
-    }
-}
-
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
